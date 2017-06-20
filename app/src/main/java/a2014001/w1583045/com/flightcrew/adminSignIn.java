5890c7842732a79d5306968650786f15e6b1a9d4
Ramathi <Ramathi.2014001@iit.ac.lk>
2017-06-20 17:23:25 +0530

    [20-July-2017 5:23PM]

---
+++ app/src/main/java/a2014001/w1583045/com/flightcrew/adminSignIn.java
@@ -0,0 +1,13 @@
+package a2014001.w1583045.com.flightcrew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class adminSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_sign_in);
+    }
+}
